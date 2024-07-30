ad25faa13e80195097c310acc02979e2aaade918
NHHo <moonnight1109@gmail.com>
2024-07-31 02:02:02 +0800

    V01

--- 088. Merge Sorted Array/Solution.py
+++ 088. Merge Sorted Array/Solution.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
+        # Runtime 19mS / Memory 11.63MB
         """
         :type nums1: List[int]
         :type m: int
@@ -9,4 +8,13 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        
+        k = 0
+        numstmp = nums1
+        for i in range(m+n):
+            if k < m:
+                numstmp[i] = nums1[k]
+                k+=1
+            else:
+                numstmp[i] = nums2[k-m]
+                k+=1
+        nums1 = numstmp.sort()
