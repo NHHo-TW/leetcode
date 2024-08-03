60331adbde415c16a9e1c372387f7936899cb801
NHHo <moonnight1109@gmail.com>
2024-08-04 02:05:00 +0800

    V02, accepted

--- 169. Majority Element/solution.cpp
+++ 169. Majority Element/solution.cpp
@@ -2,16 +2,15 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int i , j = 0 , k , n = nums.size();
-        for (i = 0 ; i < n ; i++) {
-            k = 0;
-            for (j = 0 ; j < n ; j++) {
-                if (nums[j] == nums[i]) {
-                    k++;
+        sort(nums.begin() , nums.end());
+        j = (n / 2) + 1;
+        if (nums[0] == nums[j - 1]){
+            k = nums[0];
+        } else {
+            for (i = j ; i < n ; ++i){
+                if (nums[i - j + 1] == nums[i]){
+                    k = nums[i];
                 }
-            }
-            if (k > (n/2)) {
-                k = nums[i];
-                break;
             }
         }
         return k;
