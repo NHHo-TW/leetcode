4ed9ecaa65d45c53120e27a648b969ee074847b9
NHHo <moonnight1109@gmail.com>
2024-08-04 19:44:55 +0800

    add method 2

--- 189. Rotate Array/solution.cpp
+++ 189. Rotate Array/solution.cpp
@@ -10,3 +10,17 @@ public:
         nums = temp;
     }
 };
+
+/*
+// Method 2
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int i , j , n = nums.size();
+        vector<int> temp(n);
+        k = k % n;
+        nums.insert(nums.end() , nums.begin() , nums.end() - k);
+        nums.erase(nums.begin() , nums.begin() + n - k);
+    }
+};
+*/
