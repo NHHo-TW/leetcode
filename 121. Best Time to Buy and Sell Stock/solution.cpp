a093ff570363c47c4b184c2f1ae28d0fc1b68cbc
NHHo <moonnight1109@gmail.com>
2024-08-05 22:25:58 +0800

    V03, accepted

--- 121. Best Time to Buy and Sell Stock/solution.cpp
+++ 121. Best Time to Buy and Sell Stock/solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        int i , j = -1 , k , max_profit = 0 , profit = 0;
+        int i , j = -1 , max_profit = 0 , profit = 0;
         vector<int> temp(n - 1);
         for (i = 0 ; i < n - 1 ; ++i){
             temp[i] = prices[i + 1] - prices[i];
@@ -16,15 +16,13 @@ public:
         }
 
         for (i = j ; i < n - 1 ; ++i){
-            for (k = 0 + i ; k < n - 1 ; ++k){
-                profit = profit + temp[k];
-                if (profit > max_profit) {
-                    max_profit = profit;
-                }
+            profit = profit + temp[i];
+            if (profit > max_profit) {
+                max_profit = profit;
+            } else if (profit < 0) {
+                profit = 0;
             }
-            profit = 0;
         }
-
         return max_profit;
     }
 };
