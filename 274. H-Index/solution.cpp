ef6cab68a730e26076dddacb8caa7ac772ddc3a5
NHHo <moonnight1109@gmail.com>
2024-08-11 20:40:18 +0800

    V02，accepted

--- 274. H-Index/solution.cpp
+++ 274. H-Index/solution.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        int i , h;
-        sort(citations.begin() , citations.end());
+        int i , h = 0;
+        sort(citations.begin() , citations.end() , greater<int>());
         for (i = 0 ; i < n ; ++i){
-            if (citations[i] <= n - i) {
-                h = citations[i];
+            if (citations[i] >= i + 1) {
+                h = i + 1;
             }
         }
         return h;
